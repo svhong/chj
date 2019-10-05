e99e78639ade7b24c3456df584015062d9ab4dcc
Sean <writeseanhong@gmail.com>
2019-10-04 21:37:10 -0500

    added player component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 
 import Menu from "./components/Menu";
 import Content from "./components/Content";
-
+import Player from "./components/Player";
 
 const App = () => {
     return (
         <div>
             <Menu />
             <Content />
+            <Player />
         </div>
     )
 }
